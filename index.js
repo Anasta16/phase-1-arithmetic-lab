55eb157580554d8a9a4b7ab1d196d9ca114a56e4
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-09 11:37:40 -0500

    commit tree 33a3b8b9024cc85a5876896d71526d14da5c7a2d

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1= 62;
+const num2= 1;
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 36;
+const num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(16, 17, 18, 19, 20);
